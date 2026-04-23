849b35e6faf7c98fbda84c9f0e8761014800e0bd
HcThnh <thinh.huynhhctbk22@hcmut.edu.vn>
2026-04-23 19:47:49 +0700

    Fix 401: Add missing Spring Security tables

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    username varchar(50) not null primary key,
+    password varchar(500) not null,
+    enabled boolean not null
+);
+
+CREATE TABLE IF NOT EXISTS authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS ix_auth_username on authorities (username,authority);
